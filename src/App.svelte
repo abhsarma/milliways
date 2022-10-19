5eef049447290c334c0e86ef76c222b157a13008
Kyle Hwang <60527858+hwangkyle@users.noreply.github.com>
2022-10-19 14:27:42 -0500

    reduced horizontal space the code section takes up, moved the initial popup to its own component

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,7 @@
 	import Grid from './components/Grid.svelte';
 	import ToggleSize from './components/toggle-gridSize.svelte'
 	import Code from './components/Code.svelte';
+	import Popup from './components/Popup.svelte';
 
 	// CSS Styles
 	export const container = css`
@@ -114,34 +115,6 @@
 			}
 			isSyncingRightScroll = false;
 		}
-		document.querySelector('body').style.overflow = "hidden";
-		document.querySelector('main').insertAdjacentHTML('beforeend', `
-			<div style="position:absolute;top:0;left:0;height:100vh;width:100vw;background-color:${colors.gray+"80"};z-index:3;">
-				<div 
-					id="info-popup"
-					style="
-						position:absolute;
-						top:${header.top+nameContainer.height+gridNamesHeight}px;
-						left:${document.querySelector(".button-wrapper").offsetWidth+document.querySelector(".main").offsetWidth}px;
-						width:175px;
-						white-space:initial;
-						background-color:${colors.gray};
-						color:#F6F6F6;
-						border-radius:0 10px 10px 10px;
-					"
-				>
-					<p style="margin:10px;">
-						Click on any rectangle to open a new window of the analysis document with the options of the selected row displayed.
-					</p>
-					<button
-						style="margin:10px"
-						onclick="(function(e){e.parentElement.parentElement.remove();document.querySelector('body').style.overflow='auto';})(this)"
-					>
-						Close
-					</button>
-				</div>
-			</div>
-		`);
 	});
 
 	// defining color variables for use in CSS
@@ -153,6 +126,7 @@
 </script>
 
 <main>
+	<Popup/>
 	<div id="leftDiv"></div>
 	<div class = "container-flex">
 		<div class="row">
--- src/components/Code.svelte
+++ src/components/Code.svelte
@@ -1,9 +1,8 @@
 <script>
 	import { css } from '@emotion/css';
-    import { onMount } from 'svelte';
 	import { colors } from '../utils/colorPallete.js';
     import * as code from '../../static/data/code.json';
-    import ParamOptions from './ParamOptions.svelte';
+    import CodeLine from './CodeLine.svelte';
 
     // 80 for transparency
     export const highlight = css`
@@ -12,54 +11,14 @@
 
     `;
     export const codeLine = css`margin: 0;`;
-
-    /*
-        NOTES ABOUT THE REGEX:
-        - it assumes the content in the parentheses is a valid R variable name
-        - if "branch(...)" is preceded by any \w, i.e. [a-zA-Z0-9_], it will not match.
-    */
-    const branchRegex = /(\bbranch\(([a-zA-Z]|\.[a-zA-Z_.])[\w.]*\))/;
-    const codeHTML = 
-        code.code.map(line => 
-            line.replace(
-                branchRegex,
-                `<span class='highlight ${highlight}'>$1</span>`
-        ));
-
-    $: displayParams = [];
-
-    function branchOnClick() {
-        const currBranch = this.textContent.match(/(?<=\().*(?=\))/)[0]; // get branch name
-        const index = displayParams.indexOf(currBranch);
-        if (index !== -1) {
-            displayParams.splice(index, 1);
-            this.style.backgroundColor = colors.inactive+"80";
-        }
-        else {
-            displayParams.push(currBranch);
-            this.style.backgroundColor = colors.active+"80";
-        }
-        displayParams = displayParams;
-    }
-    
-    onMount(() => {
-        document.querySelectorAll('.highlight').forEach(el => el.addEventListener('click', branchOnClick));
-    });
 </script>
 
 <div class="code-container">
     <div class="code">
         <h2>CODE</h2>
-        {#each codeHTML as line}
-            <pre class="code-line {codeLine}">{@html line}</pre>
-        {/each}
-    </div>
-    <div class="param-options-container">
-        {#each displayParams as param (param)}
-            <ParamOptions
-                param={param}
-                optionsData={code.parameters[param]}
-            />
+        <!--TODO: passing the whole code.parameters is probably bad-->
+        {#each code.code as line}
+            <CodeLine line={line} optionsData={code.parameters} />
         {/each}
     </div>
 </div>
@@ -69,9 +28,7 @@
 		display: inline-flex;
 		position: absolute;
 		margin-left: 16px;
+        margin-right: 16px;
     }
     
-    div.param-options-container {
-		margin-left: 16px;
-    }
 </style>
